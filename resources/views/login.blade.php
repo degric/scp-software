f8ae7e250cea9262775d09a75bf47e06c3ad586e
degric <ricardodegante48@gmail.com>
2024-07-02 12:36:24 -0600

    Login 1 Version

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesión</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+</head>
+<body>
+    <header>
+      
+    </header>
+    <div class="login-container">
+        <h2>Inicio de Sesión</h2>
+        <form action="#" method="POST" class="form">
+        
+            <input type="text" id="username" name="username" placeholder="Usuario " required>
+
+            <input type="password" id="password" name="password" placeholder="Contraseña " required>
+
+            <button type="submit">Iniciar Sesión</button>
+        </form>
+    </div>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/login', [LoginController::class, 'index'] );
+
+
+Route::get('/admin', function(){
+    return "Hola, si se pudo, /admin";
 });
